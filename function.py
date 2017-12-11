ebf99e635d0d3e2fd8319e409b6a7a9368769e74
aasis <asiskc143@gmail.com>
2017-12-11 08:35:08 +0545

    function

---
+++ function.py
@@ -0,0 +1,8 @@
+def odd_even(n):
+     if n % 2 == 0:
+         print ("It is an even number")
+
+     else:
+         print ("It is an odd number")
+
+         odd_even(int(input("enter a number")))
